aba03eebede7a99530331fa1ab17b711b5555d65
erajneeshverma <rajneeshverma94306@gmail.com>
2024-03-01 11:56:40 +0530

    Hero Section Completed

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
+//internal import
+import Style from '../styles/index.module.css';
+import { HeroSection } from '@/components/componentindex';
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className={Style.heroPage}>
+      <HeroSection/>
+    </div>
   )
 }
 
