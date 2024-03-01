9b394913076d95d0fe1127e23d37e4e49e51dd58
erajneeshverma <rajneeshverma94306@gmail.com>
2024-03-01 12:58:57 +0530

    service component added

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 
 //internal import
 import Style from '../styles/index.module.css';
-import { HeroSection } from '@/components/componentindex';
+import { HeroSection, Service } from '@/components/componentindex';
 
 const Home = () => {
   return (
     <div className={Style.heroPage}>
       <HeroSection/>
+      <Service/>
     </div>
   )
 }
