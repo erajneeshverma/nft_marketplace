fa3c3659fa238233d400f81dabe8dfa5778ac551
Rajneesh Kumar Verma <75884755+erajneeshverma@users.noreply.github.com>
2024-03-07 05:21:28 +0000

    Subscribe component added

--- components/componentindex.js
+++ components/componentindex.js
@@ -3,4 +3,6 @@ import Footer from "./Footer/Footer";
 import NavBar from "./Navbar/NavBar";
 import HeroSection from "./HeroSection/HeroSection";
 import Service from './Service/Service';
-export {Button,Footer,NavBar, HeroSection, Service};
+import Subscribe from "./subscribe/Subscribe";
+
+export {Button,Footer,NavBar, HeroSection, Service, Subscribe};
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,15 @@ import React from 'react'
 
 //internal import
 import Style from '../styles/index.module.css';
-import { HeroSection, Service } from '@/components/componentindex';
+import { HeroSection, Service, Subscribe } from '@/components/componentindex';
 
 const Home = () => {
   return (
     <div className={Style.heroPage}>
       <HeroSection/>
       <Service/>
+      
+      <Subscribe/>
     </div>
   )
 }
